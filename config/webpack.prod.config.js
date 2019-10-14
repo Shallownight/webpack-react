f2e39a425c1953f3bdaaff2875995e8eb820183b
shuhuai <shuhuai@pinduoduo.com>
2019-10-14 11:33:15 +0800

    feat: 构建时自动清除原打包文件

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.config.js');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
 })
 
